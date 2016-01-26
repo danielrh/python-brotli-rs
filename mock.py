d361c226f0bfbcf1d9d35e02065cf4a260d40077
Mehant Baid <mehant@dropbox.com>
2016-01-25 21:56:57 -0800

    Integrate with brotli decompression

--- mock.py
+++ mock.py
@@ -2,8 +2,20 @@ import ctypes
 from ctypes import cdll
 
 lib = cdll.LoadLibrary("target/release/libembed.dylib")
-block_dump = "ABCDE"
-print("block dump before invoking rust code: " + block_dump)
-ptr = ctypes.c_char_p(block_dump)
-length = lib.brotli_compression_mock(ptr)
-print("block dump after invoking rust code: " + block_dump + " length: " + str(length))
+
+# compressed input brotli stream
+input_compressed_stream = bytearray('\x1b\x13\x00\x00\xa4\xb0\xb2\xea\x81\x47\x02\x8a')
+input_length = len(input_compressed_stream)
+# transform it into a ctype that can be passed into rust 
+input_array = (ctypes.c_char * 12).from_buffer(input_compressed_stream)
+input_char_ptr = ctypes.cast(input_array, ctypes.c_char_p)
+
+# hard coded output buffer length
+output_length = 25
+# allocate output bytearray
+output_buffer = bytearray(output_length); 
+output_array = (ctypes.c_char * output_length).from_buffer(output_buffer)
+output_char_ptr = ctypes.cast(output_array, ctypes.c_char_p)
+
+lib.brotli_compression_mock(input_char_ptr, input_length, output_char_ptr, output_length)
+print("Decompressed string: " + output_buffer)
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,36 @@
+#![feature(raw)]
 extern crate libc;
+extern crate brotli;
+use std::io::{ Cursor, Read };
+use brotli::Decompressor;
+use std::raw::Slice;
+use std::mem;
 /*
- * Dummy function that accepts a pointer to a string from python. Mutates
- * the string and returns an integer back to python. 
+ * Function decompresses the input stream and stores it in the output buffer
+ * Args:
+ *    input_buffer_ptr: pointer to the input buffer (compressed stream)
+ *    input_length: length of the input buffer
+ *    output_buffer_ptr: pointer to the output buffer, decompressed output
+ *                       will be written to this buffer
+ *    output_length: length of the output buffer
  */ 
 #[no_mangle]
-pub extern fn brotli_compression_mock(input: &mut libc::c_char) -> i32 {
-    // mutate the string 
-    *input += 1;
-    // return the length of the mutated string. 
-    return 5;
+pub extern fn brotli_compression_mock(input_buffer_ptr: *const libc::c_char, input_length: libc::size_t, 
+                                      output_buffer_ptr: *const libc::c_char, output_length: libc::size_t) {
+    let input_slice: Slice<u8> = Slice {
+        data: input_buffer_ptr as *const u8,
+        len: input_length as usize
+    };
+
+    let input_bytes: &mut [u8] = unsafe { mem::transmute(input_slice) };
+    let input_brotli_stream = Cursor::new(input_bytes);
+
+    let output_slice: Slice<u8> = Slice {
+        data: output_buffer_ptr as *const u8,
+        len: output_length as usize
+    };
+    let output_bytes: &mut [u8] = unsafe { mem::transmute(output_slice) };
+    
+    // Invoke the brotli decompressor on the input stream, write to the output byte array
+    let _ = Decompressor::new(input_brotli_stream).read_exact(output_bytes);
 }
